77edc91ce56d053367b31868629f12cd932bfdaa
박지호 <jiho5993@naver.com>
2020-12-28 14:38:09 +0900

    BOJ_.

--- commit.sh
+++ commit.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# BOJ : ./commit.sh boj "$2" "$3"
+# Programmers : ./commit.sh programmers "$2"
 
 message=""
+flag=0
 
 boj() {
     message="BOJ_$1.$2"
@@ -10,13 +13,34 @@ programmers() {
     message="Programmers.$1"
 }
 
-if [ "$2" = "" ]
+if [ "$1" = "boj" ]
 then
-    programmers "$1"
+    if [ -z "$2" || -z "$3" ]
+    then
+        message="error: must have 2 args"
+    else
+        boj "$2" "$3"
+        flag=1
+    fi
+elif [ "$1" = "programmers" ]
+then
+    if [ -z "$2" ]
+    then
+        message="error: must have 1 args"
+    else
+        programmers "$2"
+        flag=1
+    fi
 else
-    boj "$1" "$2"
+    message="error: none type (boj, programmers)"
 fi
 
-git add .
-git commit -m "$message"
-git push -u origin master
+if [ $flag -eq 0 ]
+then
+    echo "$message"
+    exit 2
+else
+    git add .
+    git commit -m "$message"
+    git push -u origin master
+fi
